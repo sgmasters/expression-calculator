e517d925827ce57aa3342fcb47100a81cf7e6d7c
yrusak <yauheni.rusak@godeltech.com>
2020-03-19 17:49:51 +0300

    feat: complete medium and hard

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ function eval() {
 
 function expressionCalculator(expr) {
   // write your solution here
-  let s = expr.trim();
+    let s = expr.replace(/\s/g, '');
   if (!checkIfContainsBracket(s)) {
     return simpleCalc(s);
   } else {
     checkBracketsPaired(s);
-    return simpleCalc(getInnerBracketsResult(s));
+    return simpleCalc(calcComplex(s));
   }
 
   function simpleCalc(simpleExpression) {
@@ -52,9 +52,31 @@ function expressionCalculator(expr) {
       return devide;
     }
 
-    function calcSubstr(sumPart) {
+      function moveElem(array, index) {
+          array[index] = array[index] + '-' + array[index + 1];
+          return array.splice(index + 1, 1);
+      }
+
+      function checkWrongSplit(split) {
+          for (let i = 0;i<split.length;i++) {
+              let first = split[i].split('');
+              let indexSpace = split.indexOf('');
+              if (first[first.length - 1] == '/' || first[first.length - 1] == '*') {
+                  moveElem(split, i);
+                  i--;
+              } else if (indexSpace != -1) {
+                  moveElem(split, indexSpace);
+                  i--;
+              }
+          }
+      }
+
+      function calcSubstr(sumPart) {
       let splited = sumPart.split('-');
-      let substr = calcMultiply(splited[0]);
+        let substr;
+        checkWrongSplit(splited);
+
+        substr = calcMultiply(splited[0]);
       if (splited.length > 1) {
         for (let i = 1; i < splited.length; i++) {
           substr -= calcMultiply(splited[i]);
@@ -79,15 +101,56 @@ function expressionCalculator(expr) {
   function getInnerBracketsResult(brackets) {
     let firstBr = brackets.indexOf('(');
     let endBr = brackets.lastIndexOf(')');
-
-    let innerBrackets = brackets.substr(firstBr, endBr);
+    let innerBrackets = brackets.slice(firstBr+1, endBr);
     let result;
     if (checkIfContainsBracket(innerBrackets)) {
       getInnerBracketsResult(innerBrackets);
     } else {
       result = simpleCalc(innerBrackets);
     }
-    return brackets.replace(innerBrackets, result);
+      return brackets.replace('(' + innerBrackets + ')', result);
+  }
+
+    function doSmall(string, opens, closes) {
+        let simpleBrackets = string.substring(opens, closes + opens + 2);
+        let res = string.replace(simpleBrackets, getInnerBracketsResult(simpleBrackets));
+        return res;
+    }
+
+    function rowBrackets(str) {
+      let open = str.indexOf('(');
+      let nextOpen = str.substr(open+1).indexOf('(');
+      let nextClose = str.substr(open+1).indexOf(')');
+      let result;
+      if (nextOpen == -1 && nextClose == -1) {
+        result = '' + simpleCalc(str);
+      } else if (nextClose < nextOpen || nextOpen == -1) {
+        result = rowBrackets(doSmall(str, open, nextClose));
+      } else if (nextOpen < nextClose) {
+        let lastClose = str.lastIndexOf(')');
+        let prevLastClose = str.slice(0, lastClose).lastIndexOf(')');
+        let lastOpen = str.lastIndexOf('(');
+        if (lastOpen < prevLastClose) {
+            result = rowBrackets(doSmall(str, lastOpen, lastClose));
+        } else {
+            let complexBrackets = str.substring(open + 1, lastClose);
+            result = str.replace('(' + complexBrackets + ')', simpleCalc(rowBrackets(complexBrackets)));
+        }
+      }
+      return result;
+  }
+
+  function calcComplex(ss) {
+      let firstClose = ss.indexOf(')');
+      let lastOpen = ss.slice(0,firstClose).lastIndexOf('(');
+      let block = ss.slice(lastOpen, firstClose + 1);
+      let result;
+      if (firstClose == -1 && lastOpen == -1) {
+          result = '' + simpleCalc(ss);
+      } else {
+        result = calcComplex(ss.replace(block, getInnerBracketsResult(block)));
+      }
+      return result;
   }
 }
 
